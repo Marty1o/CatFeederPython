f0e3d94c145d939d8d759cb3a4975486157b525c
Marty1o <52226629+Marty1o@users.noreply.github.com>
2020-09-20 00:04:01 -0700

    working version of motor and schedule

--- Motor.py
+++ Motor.py
@@ -7,7 +7,6 @@ import schedule
 class Motor:
     @staticmethod
     def runMotor():
-
         servoPIN = 17
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(servoPIN, GPIO.OUT)
@@ -15,27 +14,11 @@ class Motor:
         p = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
         p.start(2.5)  # Initialization
 
-        p.ChangeDutyCycle(5)
-        time.sleep(2)
-        p.ChangeDutyCycle(7.5)
-        time.sleep(2)
-        p.ChangeDutyCycle(10)
-        time.sleep(2)
         p.ChangeDutyCycle(12.5)
-        time.sleep(2)
-        p.ChangeDutyCycle(10)
-        time.sleep(2)
-        p.ChangeDutyCycle(7.5)
-        time.sleep(2)
-        p.ChangeDutyCycle(5)
-        time.sleep(2)
+        time.sleep(0.5)
+
         p.ChangeDutyCycle(2.5)
-        time.sleep(2)
+        time.sleep(0.5)
 
         p.stop()
         GPIO.cleanup()
-
-
-
-
-
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# from Email import *
 from Motor import *
 import schedule
 import time
@@ -7,27 +6,17 @@ import time
 def initialization():
     motor = Motor()
 
-    schedule.every(1).minute.do(Motor.runMotor)
+    schedule.every().day.at('07:00').do(Motor.runMotor)
+    schedule.every().day.at('18:00').do(Motor.runMotor)
 
-    '''print("Auto-Cat initializing")
-    email = Email(465, 'catfeederprojectpie@gmail.com', 'none at the moment')
-    # motor = Motor(21, 50, 5)
-    # schedule.every(1).minutes.do(email.sendEmail,'martin.c.842@gmail.com','This is a test')
-    email.sendEmail('martin.c.842@gmail.com', 'This is a test!!')
-    # schedule.every(1).minutes.do(motor.feed, 10, 5)
-    # Add any other initializing components here
-    print("Auto-Cat initializing complete")'''
-
-def helloWorld():
-    print("hi hi hi")
 
 def main():
     # Call initilization function
+    Motor.runMotor()
     initialization()
 
     # will loop forever and check if there are any pending tasks.
     while True:
-        print('System is running')
         schedule.run_pending()
         time.sleep(1)
 
